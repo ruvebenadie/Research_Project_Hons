250f2755030b232be84f101f652a40102cefca8a
ruvebenadie <89631736+ruvebenadie@users.noreply.github.com>
2022-10-18 10:15:25 +0200

    Update Khayelitsha_Script.py

--- Khayelitsha_Script.py
+++ Khayelitsha_Script.py
@@ -1,4 +1,4 @@
-﻿# coding: utf-8
+# coding: utf-8
 import arcpy
 import os
 #set workspace
@@ -18,12 +18,12 @@ Alley = arcpy.SelectLayerByAttribute_management("Polygon_23_MA", "NEW_SELECTION"
 Alley = arcpy.management.CopyFeatures("Polygon_23_MA", "MyProject26.gdb/Alley")
 
 #Singlepart to multipart
-Single_Foot = arcpy.MultipartToSinglepart_management("Footpath",  "MyProject26.gdb/single_foot")
-Single_Vech = arcpy.MultipartToSinglepart_management("Vehicular",  "MyProject26.gdb/single_vech")
-centerline_foot = arcpy.topographic.PolygonToCenterline("single_foot", "MyProject26.gdb/foot_cnrline")
+single_foot = arcpy.MultipartToSinglepart_management("Footpath",  "MyProject26.gdb/single_foot")
+single_vech = arcpy.MultipartToSinglepart_management("Vehicular",  "MyProject26.gdb/single_vech")
+single_alley = arcpy.MultipartToSinglepart_management("Alley",  "MyProject26.gdb/single_alley")
 
 #Extract centrelines
-centerline_alley = arcpy.topographic.PolygonToCenterline("Alley", "MyProject26.gdb/alley_cnrline")
+centerline_foot = arcpy.topographic.PolygonToCenterline("single_foot", "MyProject26.gdb/foot_cnrline")
 centerline_vech = arcpy.topographic.PolygonToCenterline("single_vech", "MyProject26.gdb/vech_cnrline")
 centerline_alley = arcpy.topographic.PolygonToCenterline("single_alley", "MyProject26.gdb/alley_cnrline")
 
@@ -31,3 +31,4 @@ centerline_alley = arcpy.topographic.PolygonToCenterline("single_alley", "MyProj
 clean_footpath = arcpy.analysis.Buffer("foot_cnrline", "MyProject26.gdb/clean_footpath", "0.2 meters", "Full", "Round", "All")
 clean_alley = arcpy.analysis.Buffer("alley_cnrline", "MyProject26.gdb/clean_alley", "0.2 meters", "Full", "Round", "All")
 clean_vehicular = arcpy.analysis.Buffer("vech_cnrline", "MyProject26.gdb/clean_vehicular", "1 meters", "Full", "Round", "All")
+
